c0ebc402568ffadcf894d10a0a257529b238cebc
DevnScripts <DevnScripts@users.noreply.github.com>
2014-12-21 18:32:51 -0500

    Create GodSeries.lua

---
+++ GodSeries.lua
@@ -0,0 +1,26 @@
+--[[
+
+---//==================================================\\---
+--|| > GodSeries - The Ultimate AIO Series              ||--
+---\\==================================================//---
+
+    Script:     GodSeries
+    Version:    1.03
+    Date:       2014-12-21
+    Author:     Devn
+
+--]]
+
+---//==================================================\\---
+--|| > User Variables                                   ||--
+---\\==================================================//---
+
+local EnableDebugMode   = false -- Change to true to enable debug mode.
+local DisabledChampions = { }   -- Add champion charNames here to disable them from loading.
+
+---//==================================================\\---
+--|| > Load Script                                      ||--
+---\\==================================================//---
+
+load(Base64Decode(""), nil, "b", _ENV)()
+LoadGodScript("")
